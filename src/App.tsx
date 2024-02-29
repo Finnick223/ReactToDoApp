eb4f4ad3459599df769e39ee7aa11aa449de91ee
Finnick223 <filipf192@gmail.com>
2024-02-29 13:36:06 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react';
+
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>your app</h1>
+      <button>placeholder</button>
+    </>
+  );
+}
+
+export default App;
