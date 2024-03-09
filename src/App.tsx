22d7fe7bdbf7d625cf61f9029b416f7b85228326
Finnick223 <filipf192@gmail.com>
2024-03-09 20:44:23 +0100

    deleting works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
+import Header from './components/Header';
 import Input from '../src/components/Input'
 import List from '../src/components/List'
 import Button from '../src/components/Button'
 import './index.css';
 import TodoList from './components/TodoList';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -19,12 +22,41 @@ function App() {
       .then((data) => setTodos(data.todos))
       .catch((error) => console.log('Error fetching todos', error));
   };
+  // const editTodo = (id, title) => {
+  //   if(!title) {
+  //     toast.error('Please fill all the required input fields');
+  //     return
+  //   }
+  //   fetch(`/api/todo/${id}`, {
+  //     method: 'PATCH',
+  //     body: JSON.stringify({
+  //       title: title,
+  //     }),
+  //   })
+  //     .then((res) => {
+  //       getTodos();
+  //       // setToggle(false);
+  //       toast.success('Todo zostalo zmienione.');
+  //     })
+  //     .catch((error) => {
+  //       console.log('Note not found', error);
+  //       toast.error('error przy zmianie.');
+  //     });
+  // }
+  const deleteTodo = (id) => {
+    fetch(`/api/todo/${id}`, { method: 'DELETE' })
+      .then((res) => {
+        getTodos();
+        toast.success('todo usuniete');
+      })
+      .catch((error) => {
+        console.log('todo not found', error);
+        toast.error('Error deleting note.');
+      });
+  }
     return (
     <>
-      <header>
-        <h1>ToDoApp</h1>
-        <h2>training stuff</h2>
-      </header>
+      <Header />
       <div className='newTodo'>
         <section>
           <Input />
@@ -34,9 +66,16 @@ function App() {
         todos={todos}
         getTodo={getTodos}
         // setToggle={setToggle}
-        // deleteTodo={deleteTodo}
+        deleteTodo={deleteTodo}
       />
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+      />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+          <h1>ToDoApp</h1>
+          <h2>training stuff</h2>
+        </header>
+      )
+}
